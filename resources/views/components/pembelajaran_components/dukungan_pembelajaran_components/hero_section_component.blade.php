a1284f878dec3fbf08ebbec5db59f77f59e1728a
superevilstockholm <balilombok11@gmail.com>
2025-09-01 23:33:16 +0700

    dukungan pembelajaran hero section (WIP)

---
+++ resources/views/components/pembelajaran_components/dukungan_pembelajaran_components/hero_section_component.blade.php
@@ -0,0 +1,30 @@
+<section class="mt-3">
+    <div class="container">
+        <h1 class="text-center mb-5 py-2 text-muted fw-normal">Dukungan Pembelajaran</h1>
+        <div class="row align-items-center">
+            <div class="col-12 col-md-6 col-lg-6 mb-4 mb-md-0">
+                <img class="img-fluid rounded"
+                    src="https://sph.edu/wp-content/uploads/2022/02/header-sph-academic-learningSupport-v1.jpg"
+                    alt="">
+            </div>
+            <div class="col-lg-1 d-none d-lg-block"></div>
+            <div class="col-12 col-md-6 col-lg-4">
+                <p class="fw-semibold mb-4 text-muted">Pengajar kami berusaha lebih keras dengan memperluas dukungan
+                    mereka di luar ruang kelas. Kami menyediakan program dukungan individu di berbagai bidang untuk
+                    memenuhi kebutuhan masing-masing siswa.</p>
+                <ul class="list-unstyled d-flex flex-column gap-2">
+                    <li class="d-flex align-items-center gap-1 text-muted">
+                        <img height="20" src="https://sph.edu/wp-content/uploads/2022/02/SPH-Rectangle-black.png" alt="">
+                        <a class="text-primary" style="text-decoration: none;" href="#">Dukungan Pembelajaran Tambahan</a>
+                    </li>
+                    <li class="d-flex align-items-center gap-1 text-muted">
+                        <img height="20" src="https://sph.edu/wp-content/uploads/2022/02/SPH-Rectangle-black.png" alt="">
+                        <a class="text-primary" style="text-decoration: none;" href="#">Bahasa Inggris sebagai Bahasa Tambahan
+</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-lg-1 d-none d-lg-block"></div>
+        </div>
+    </div>
+</section>
--- resources/views/pages/pembelajaran/dukungan_pembelajaran.blade.php
+++ resources/views/pages/pembelajaran/dukungan_pembelajaran.blade.php
@@ -1,7 +1,36 @@
 @extends('App')
 @section('title', 'Dukungan Pembelajaran')
 @section('content')
-<section>
-
-</section>
+    {{-- Breadcrumb --}}
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="/">Home</a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="/pembelajaran">Pembelajaran</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Dukungan Pembelajaran
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    {{-- Hero Section --}}
+    @include('components.pembelajaran_components.dukungan_pembelajaran_components.hero_section_component')
+    {{-- Parenting Quesstion Component --}}
+    @include('components.pareting_question_component')
+    {{-- Custom NavBar --}}
+    <style>
+        .navbar-container .nav-link,
+        .navbar-container .navbar-brand {
+            color: #000 !important;
+            /* Teks hitam setelah scroll */
+        }
+    </style>
 @endsection
