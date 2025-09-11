a12c187a296cb03e4d3cc5586fad4baeb650cc24
superevilstockholm <balilombok11@gmail.com>
2025-09-11 10:59:26 +0700

    Created publikasi page

---
+++ resources/views/pages/berita_dan_acara/publication.blade.php
@@ -0,0 +1,41 @@
+@extends('App')
+@section('title', 'Acara')
+@section('content')
+    {{-- Breadcrumb --}}
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a href="/">Home</a>
+                        </li>
+                        <li class="breadcrumb-item">
+                            <a href="/berita-dan-acara">Berita dan Acara</a>
+                        </li>
+                        <li class="breadcrumb-item active" aria-current="page">
+                            Publikasi
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+    {{-- Hero Section --}}
+    @include('components.berita_dan_acara_components.publikasi_components.hero_section_component')
+    {{-- Publikasi Section --}}
+    @include('components.berita_dan_acara_components.publikasi_components.publikasi_section_component')
+    {{-- Parenting Quesstion Component --}}
+    @include('components.pareting_question_component')
+    {{-- Custom NavBar --}}
+    <style>
+        .navbar-container nav .nav-link,
+        .navbar-container nav .navbar-brand {
+            color: rgba(0, 0, 0, 0.75) !important;
+            /* Teks hitam setelah scroll */
+        }
+        .navbar-container nav .nav-link:hover {
+            color: rgba(0, 0, 0, 1) !important;
+        }
+    </style>
+@endsection
