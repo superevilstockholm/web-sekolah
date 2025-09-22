39b82b92743efd5f749144473d56cf4e8a7edf1d
superevilstockholm <balilombok11@gmail.com>
2025-09-22 14:16:34 +0700

    Created Login authentication and CRUD operation for users

---
+++ app/Http/Controllers/MasterData/UserController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers\MasterData;
+
+use App\Http\Controllers\Controller;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            // Initialize
+            $users = User::with('roles');
+            // Search (harus sebelum get/paginate)
+            if ($request->filled('type') && $request->filled('query')) {
+                $users->where($request->type, 'like', '%' . $request->query . '%');
+            }
+            // Limit
+            $limit = $request->query('limit', 10);
+            if ($limit === 'all') {
+                $users = $users->get();
+            } else {
+                $users = $users->paginate((int) $limit);
+            }
+            // Response
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $users
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required|max:255',
+                'email' => 'required|email|unique:users',
+                'password' => 'required|min:8'
+            ]);
+
+            $validated['password'] = Hash::make($validated['password']);
+
+            $user = User::create($validated);
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $user
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user): JsonResponse
+    {
+        try {
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $user
+            ]);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error',
+                'error' => $e->getMessage()
+            , 500]);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, User $user): JsonResponse
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'sometimes|max:255',
+                'email' => 'sometimes|email|unique:users,email,' . $user->id,
+                'password' => 'sometimes|min:8'
+            ]);
+            if (isset($validated['password'])) {
+                $validated['password'] = Hash::make($validated['password']);
+            }
+            $user->update($validated);
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $user->refresh()
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user): JsonResponse
+    {
+        try {
+            $user->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Success',
+                'data' => $user
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Error',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Auth
+use App\Http\Controllers\AuthController;
+
+// Master Data
+use App\Http\Controllers\MasterData\UserController;
+
+// Auth
+Route::post('login', [AuthController::class, 'login']);
 
 // Protected Routes
 Route::middleware(['auth:sanctum'])->group(function () {
-
+    Route::apiResource('users', UserController::class)->parameters([
+        'users' => 'user'
+    ]);
 });
